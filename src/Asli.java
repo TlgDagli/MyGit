6f68f4e20af03e0f8947079a284543bec8235383
ArcCode <cinucevasli@gmail.com>
2024-03-01 19:20:46 +0100

    asli

---
+++ src/Asli.java
@@ -0,0 +1,12 @@
+import Utility.BaseDriver;
+import org.junit.Test;
+
+import java.util.prefs.BackingStoreException;
+
+public class Asli extends BaseDriver {
+    @Test
+
+    public void test1(){
+
+    }
+}
